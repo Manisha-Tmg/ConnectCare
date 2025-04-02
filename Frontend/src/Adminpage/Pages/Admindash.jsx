694d6177ee951b942dfdb6b9e37f9a9ac7ce3349
manishatamang752@gmail.com <manishatamang752@gmail.com>
2025-04-02 23:42:24 +0545

    added count feat in backend

--- Frontend/src/Adminpage/Pages/Admindash.jsx
+++ Frontend/src/Adminpage/Pages/Admindash.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../Css/admindashboard.css";
+import Cookies from "js-cookie";
+import { FaCalendarCheck, FaTasks, FaUserClock } from "react-icons/fa";
 
+const Dashboard = () => {
+  const [data, setdata] = useState();
+  const [totalbooking, setTotalbooking] = useState([]);
+
+  useEffect(() => {
+    async function handledash() {
+      const token = Cookies.get("accessToken");
+
+      try {
+        const res = await fetch(`${API}caretaker/dashboard/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await res.json();
+
+        setdata(data);
+      } catch (error) {}
+    }
+    handledash();
+  }, []);
+};
 const Admindash = () => {
-  return <div>Admindash</div>;
+  <div>
+    <div className="dashboard-container">
+      <h2 className="dashboard-header">
+        {/* <Previous /> */}
+        Dashboard
+      </h2>
+
+      <section className="overview">
+        <div className="card">
+          <div className="card-icon-wrapper">
+            <FaCalendarCheck className="card-icon" />
+          </div>
+          <h2>Total Bookings</h2>
+          <p className="task-count">{data?.total_bookings || 0}</p>
+        </div>
+        <div className="card">
+          <div className="card-icon-wrapper">
+            <FaTasks className="card-icon" />
+          </div>
+          <h2>Pending Tasks</h2>
+          <p className="task-count">{data?.pending_tasks || 0}</p>
+        </div>
+        <div className="card">
+          <div className="card-icon-wrapper">
+            <FaUserClock className="card-icon" />
+          </div>
+          <h2>Completed</h2>
+          <p className="task-count">{data?.completed_tasks || 0}</p>
+        </div>
+      </section>
+    </div>
+  </div>;
 };
 
 export default Admindash;
