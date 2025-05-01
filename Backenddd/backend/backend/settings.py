c04b46b405a4557253615bf4c522bd6d29bdeba0
manishatamang752@gmail.com <manishatamang752@gmail.com>
2025-05-01 22:30:05 +0545

    Host email changes

--- Backenddd/backend/backend/settings.py
+++ Backenddd/backend/backend/settings.py
@@ -124,8 +124,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'manshaa4545@gmail.com'
-EMAIL_HOST_PASSWORD = 'qyah qznl nyeq mwop'  
+EMAIL_HOST_USER = 'ishabhatta41@gmail.com'
+EMAIL_HOST_PASSWORD = 'fckb cuxy mszo iawh'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
