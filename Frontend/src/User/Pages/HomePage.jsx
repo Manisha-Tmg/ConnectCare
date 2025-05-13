514fc3584ce05f5cbdd112bd86599f98b061dc31
manishatamang752@gmail.com <manishatamang752@gmail.com>
2025-05-13 12:00:00 +0545

    Updated Homepage Css

--- Frontend/src/User/Pages/HomePage.jsx
+++ Frontend/src/User/Pages/HomePage.jsx
@@ -4,14 +4,12 @@ import Footer from "../components/Footer";
 import Contact from "./Contact";
 import HomePage from "./Home";
 import "../css/AboutUs.css";
-import Caretakers from "./Caretaker";
 
 const Home = () => {
   return (
     <div className="main-div">
       <Header />
       <HomePage />
-      {/* <Caretakers /> */}
       <Contact />
       <Footer />
     </div>
